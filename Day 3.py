a7a7a4a9e6ca1beb1e20d5383cda0bccfe9b282b
Saleh Akram <116333689+saleh-akram-rafi@users.noreply.github.com>
2026-09-13 23:16:19 +0600

    Add Python learning exercises

---
+++ Day 3.py
@@ -0,0 +1,24 @@
+# developer = 'Devin'
+
+#print(type(developer)) # This will print <class 'str'> since developer is a string
+
+#my_integer_var = 10
+#print(type(my_integer_var))  # <class 'int'>
+
+#my_float_var = 4.50
+#print(type(my_float_var))  # <class 'float'>
+
+#my_string_var = 'hello'
+#print(type(my_string_var))  # <class 'str'>
+
+#my_boolean_var = True
+#print(type(my_boolean_var))  # <class 'bool'>
+
+
+account_balance = '12'
+account_balance1 = 12
+
+isinstance(account_balance, int) # False
+isinstance(account_balance1, (int, float)) # True
+
+
