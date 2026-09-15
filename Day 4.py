33d1b2a3746fc984976b9b8c533784cc24d2900d
Saleh Akram <116333689+saleh-akram-rafi@users.noreply.github.com>
2026-09-15 22:55:01 +0600

    Add learning update

--- Day 4.py
+++ Day 4.py
@@ -28,3 +28,13 @@ quote2 = "She said, \"Hello!\""  #using different quotation mark
 print(msg)
 print(quote2)
 
+
+#Uing in operatior
+
+my_str = 'Hello world'
+
+print('Hello' in my_str)  # True
+print('hey' in my_str)    # False
+print('hi' in my_str)    # False
+print('e' in my_str)  # True
+print('f' in my_str)  # False
