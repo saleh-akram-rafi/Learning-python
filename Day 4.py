36d2fc1450f1aa182bfac0328addf626983c1a4c
Saleh Akram <116333689+saleh-akram-rafi@users.noreply.github.com>
2026-09-15 22:47:19 +0600

    Add Day 4 Python learning exercise

---
+++ Day 4.py
@@ -0,0 +1,24 @@
+str1 ='hello'
+str2 ="world!" #single line string
+
+print(str1)
+print(str2)
+
+str3= """The sun sets.
+pick the phone.""" #Multiple line string.
+
+str4 = '''Another
+multiline
+string'''
+
+print(str3)
+
+print(str4)
+
+str5 = "It's a rainny day."
+
+print(str5)
+
+quote = 'She said, "Hello World!"'
+print(quote)
+
